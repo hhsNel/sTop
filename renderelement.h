20e29b7e7ad0b0e605b6e847f288a571ed51a51c
hhsNel <hihijsonel@github.com>
2025-03-09 22:07:36 +0100

    display structure moved to dspelement

--- renderelement.h
+++ renderelement.h
@@ -1,32 +1,28 @@
-size_t render_element(struct dspelement *menu, size_t i, size_t menu_size, size_t w, size_t h, size_t sw, size_t *prev_y);
-size_t render_plaintext(size_t sx, size_t sy, size_t ex, size_t ey, char *text);
-size_t render_button(size_t sx, size_t sy, size_t ex, size_t ey, char *btn_text);
-size_t render_graph_over_time(size_t sx, size_t sy, size_t ex, size_t ey, void **mem, value_getter vget);
-size_t render_divider(size_t sx, size_t sy, size_t ex, size_t ey, struct dspelement *menu, size_t i, size_t menu_size);
-size_t render_value(size_t sx, size_t sy, size_t ex, size_t ey, value_getter vget);
+size_t render_element(struct dspelement *menu, size_t i, size_t menu_size);
+size_t render_plaintext(struct dspelement *menu, size_t i, size_t menu_size);
+size_t render_button(struct dspelement *menu, size_t i, size_t menu_size);
+size_t render_graph_over_time(struct dspelement *menu, size_t i, size_t menu_size);
+size_t render_divider(struct dspelement *menu, size_t i, size_t menu_size);
+size_t render_value(struct dspelement *menu, size_t i, size_t menu_size);
 
-size_t render_element(struct dspelement *menu, size_t i, size_t menu_size, size_t w, size_t h, size_t sw, size_t *prev_y) {
+size_t render_element(struct dspelement *menu, size_t i, size_t menu_size) {
 	size_t r;
 	
 	switch(menu[i].type) {
 		case EL_PLAINTEXT:
-			r = render_plaintext(sw, *prev_y, w, i * h / menu_size, (char *)menu[i].args.v);
-			*prev_y = i * h / menu_size;
+			r = render_plaintext(menu, i, menu_size);
 			break;
 		case EL_MENU_BUTTON:
-			r = render_button(sw, *prev_y, w, i * h / menu_size, (char *)menu[i].args.v);
-			*prev_y = i * h / menu_size;
+			r = render_button(menu, i, menu_size);
 			break;
 		case EL_GRAPH_OVER_TIME:
-			r = render_graph_over_time(sw, *prev_y, w, i * h / menu_size, &menu[i].args.v, menu[i].func);
-			*prev_y = i * h / menu_size;
+			r = render_graph_over_time(menu, i, menu_size);
 			break;
 		case EL_DIVIDER:
-			r = render_divider(sw, *prev_y, w, i * h / menu_size, menu, i, menu_size);
+			r = render_divider(menu, i, menu_size);
 			break;
 		case EL_VALUE:
-			r = render_value(sw, *prev_y, w, i * h / menu_size, menu[i].func);
-			*prev_y = i * h / menu_size;
+			r = render_value(menu, i, menu_size);
 			break;
 	}
 	return r;
--- rendermenu.h
+++ rendermenu.h
@@ -1,12 +1,9 @@
 void render_menu(struct dspelement *menu, size_t menu_size, size_t w, size_t h);
 
 void render_menu(struct dspelement *menu, size_t menu_size, size_t w, size_t h) {
-	size_t i, this_y, prev_y = 0;
+	size_t i;
 
 	for(i = 0; i < menu_size;) {
-		this_y = i * h / menu_size;
-		i += render_element(menu, i, menu_size, w, h, &prev_y);
-
-		prev_y = this_y;
+		i += render_element(menu, i, menu_size);
 	}
 }
