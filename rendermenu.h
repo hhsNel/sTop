6847f6aa41723e5c2839499ef68a5f56c402188c
hhsNel <hihijsonel@github.com>
2025-03-09 22:18:33 +0100

    init_menu, temp deleted screen size logic

--- rendermenu.h
+++ rendermenu.h
@@ -1,4 +1,6 @@
 void render_menu(struct dspelement *menu, size_t menu_size, size_t w, size_t h);
+void init_menu(struct dspelement *menu, size_t menu_size, size_t w, size_t h);
+void unload_menu(struct dspelement *menu, size_t menu_size);
 
 void render_menu(struct dspelement *menu, size_t menu_size, size_t w, size_t h) {
 	size_t i;
@@ -7,3 +9,12 @@ void render_menu(struct dspelement *menu, size_t menu_size, size_t w, size_t h) 
 		i += render_element(menu, i, menu_size);
 	}
 }
+
+void init_menu(struct dspelement *menu, size_t menu_size, size_t w, size_t h) {
+	size_t i;
+
+	for(i = 0; i < menu_size; ++i) {
+		init_element(menu + i, w, h);
+	}
+}
+
--- screen.h
+++ screen.h
@@ -4,17 +4,4 @@
 
 size_t screen_w = 0;
 size_t screen_h = 0;
-char *output_buff;
-void update_screen_dims();
-static void regenerate_screen(size_t w, size_t h);
 
-void update_screen_dims() {
-	struct winsize w;
-	ioctl(0, TIOCGWINSZ, &w);
-	if(w.ws_row != screen_h || w.ws_col != screen_w) {
-		regenerate_screen(w.ws_col, w.ws_row);
-	}
-}
-static void regenerate_screen(size_t w, size_t h) {
-	
-}
