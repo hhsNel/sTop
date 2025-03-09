eff4ef1a53afca0e298d7894ec8899312d1b8326
hhsNel <hihijsonel@github.com>
2025-03-09 21:59:17 +0100

    element display

--- dspelement.h
+++ dspelement.h
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #define EL_PLAINTEXT 0
 #define EL_MENU_BUTTON 1
 #define EL_GRAPH_OVER_TIME 2
@@ -13,5 +15,36 @@ struct dspelement {
 	int type;
 	struct elargs args;
 	void *func;
+
 	char **prev_buff;
+	char **buff;
+	size_t w, h;
 };
+
+void init_element(struct dspelement *element, size_t w, size_t h);
+void write_to_element(struct dspelement *element, size_t *x, size_t *y, char *str);
+
+void init_element(struct dspelement *element, size_t w, size_t h) {
+	element->w = w;
+	element->h = h;
+	element->prev_buff = (char **)malloc(sizeof(char *) * h);
+	element->buff = (char **)malloc(sizeof(char *) * h);
+	for(size_t i = 0; i < h; ++i) {
+		element->prev_buff[i] = (char *)malloc(sizeof(char) * w);
+		element->buff[i] = (char *)malloc(sizeof(char) * w);
+	}
+}
+
+void write_to_element(struct dspelement *element, size_t *x, size_t *y, char *str) {
+	while(*str) {
+		element->buff[*y][*x] = *str;
+		++(*x);
+		*x %= element->w;
+		if(x == 0) {
+			++(*y);
+			if(*y == element->h) return;
+		}
+		++str;
+	}
+}
+
